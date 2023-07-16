e90bb8473de920463305340c7c473601066e93ef
kubertX <ikubert@163.com>
2023-07-17 00:04:48 +0800

    build(工程依赖): 依赖工程搭建

---
+++ mallX-admin/src/main/java/com/kubertX/mallX/admin/MallXAdminApplication.java
@@ -0,0 +1,12 @@
+package com.kubertX.mallX.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallXAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallXAdminApplication.class,args);
+    }
+}
